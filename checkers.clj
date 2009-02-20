7d23b548dff75298c04568bd7c042206e7240597
Frederick Polgardy <fred@polgardy.com>
2009-02-20 17:07:50 -0600

    cleaning up formatting code

--- checkers.clj
+++ checkers.clj
@@ -35,9 +35,9 @@
       (reduce str
         (concat
           (for [y (reverse (range 8))]
-            (let [s (map (partial get char-map) (get board y))]
-              (apply (partial format "%d %s %s %s %s %s %s %s %s\n" y) s)))
-          '("  0 1 2 3 4 5 6 7\n"))))))
+            (let [s (map char-map (get board y))]
+              (apply format "%d %s %s %s %s %s %s %s %s\n" (cons y s))))
+          (list "  0 1 2 3 4 5 6 7\n"))))))
 
 ;; (println (dump-board))
 
