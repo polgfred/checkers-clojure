3a218f4f04a345e72ec133d70a32754691c15fb0
Frederick Polgardy <fred@polgardy.com>
2009-02-23 22:35:35 -0600

    rendering a stupid table

--- web/checkers_server.clj
+++ web/checkers_server.clj
@@ -2,18 +2,35 @@
   (:use [compojure.html :only (html)])
   (:use [compojure.http.servlet :only (defservlet)])
   (:use [compojure.http.routes :only (GET POST)])
-  (:use [compojure.server.jetty :only (defserver start)]))
+  (:use [compojure.server.jetty :only (defserver start)])
+  (:use checkers.rules)
+  (:use checkers.player))
 
-(def bow)
+(defn board-table
+  []
+  (let [side +black+
+        board [[  1  0  1  0  1  0  1  0  ]
+               [  0  1  0  1  0  1  0  1  ]
+               [  1  0  1  0  1  0  1  0  ]
+               [  0  0  0  0  0  0  0  0  ]
+               [  0  0  0  0  0  0  0  0  ]
+               [  0 -1  0 -1  0 -1  0 -1  ]
+               [ -1  0 -1  0 -1  0 -1  0  ]
+               [  0 -1  0 -1  0 -1  0 -1  ]]]
+    (with-position [side board]
+      [:table
+        (for [row *board*]
+          [:tr
+            (for [cell row] [:td cell])])])))
 
 
 (defservlet checkers-servlet
   (GET "/checkers"
     [{"Content-Type" "text/html"}
-      (html
-        [:h1 "Hello World"])]))
+      (html (board-table))]))
 
-(defserver checkers-server {:port 9090}
+(defserver checkers-server
+  {:port 9090}
   "/*" checkers-servlet)
 
 (start checkers-server)
