e10edbd69e8e61d5db82766e5525f62e4f2fb722
Frederick Polgardy <fred@polgardy.com>
2009-02-26 08:05:19 -0600

    html/css for graphical board

--- web/page_helpers.clj
+++ web/page_helpers.clj
@@ -9,6 +9,15 @@
   []
   (javascript-tag (str "var plays = " (json (my-plays)) ";")))
 
+(let [image-map {1 "pb.png" 2 "kb.png" -1 "pr.png" -2 "kr.png"}]
+  (defn table-cell
+    [x y p]
+    (if (playable? x y)
+      [:td.on
+        (let [image-name (image-map p)]
+          (if image-name [:img {:src (str "/checkers/s/images/" image-name)}] ""))]
+      [:td.off ""])))
+
 (defn board-table
   []
   (let [side +black+
@@ -20,23 +29,23 @@
                [  0 -1  0 -1  0 -1  0 -1  ]
                [ -1  0 -1  0 -1  0 -1  0  ]
                [  0 -1  0 -1  0 -1  0 -1  ]]]
-    (with-position [side board]
-      [:div#board
-        (plays-javascript)
-        [:table
-          (for [row *board*]
-            [:tr
-              (for [cell row] [:td cell])])]])))
+    [:div#board-container
+      (with-position [side board] (plays-javascript))
+      [:table#board
+        (for [y (reverse (range +size+))]
+          [:tr
+            (for [x (range +size+)]
+              (table-cell x y (get-p x y board)))])]]))
 
 (defn main-layout
   [content]
   (html (doctype :html4)
-    [:html
+    (xhtml-tag "en"
       [:head
         [:title "Checkers"]
         (include-css "/checkers/s/css/checkers.css")
         (include-js "/checkers/s/js/checkers.js")]
       [:body
         [:h2 "checkers-clojure"]
-        content]]))
+        content])))
 
