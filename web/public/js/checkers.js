6956f934d5cb5f193c8f65fd5210561de218e46c
Frederick Polgardy <fred@polgardy.com>
2009-03-11 06:42:25 -0500

    dnd source defers to game object for move validation

--- web/public/js/checkers.js
+++ web/public/js/checkers.js
@@ -3,37 +3,23 @@ dojo.declare('Game', null, {
     // keep track of the current position
     this._side = side;
     this._board = board;
+    // this._playMap = {};
+  },
+  getPiece: function(x, y) {
+    return this._board[y][x];
   },
   doMove: function(from, to) {
     // perform the move
-  }
-});
-
-dojo.require('dojo.dnd.Source');
-
-dojo.declare('SquareSource', dojo.dnd.Source, {
-  singular: true, // don't allow multiple selection regardless of key state
-  autoSync: true, // always sync up node list with square contents
-  constructor: function(node, params) {
-    // keep track of the coords for this square
-    this._coords = params.coords;
-    this._coordsKey = params.coords.toString();
   },
-  copyState: function() {
-    // never allow pieces to be copied
-    return false;
+  isPlay: function(from, to) {
+    // whether this is a valid move
+    return (this._playMap[from] || dojo.dnd._empty)[to];
   },
-  checkAcceptance: function(source) {
-    // return whether the drop is a valid move
-    // see _playMap below
-    return (this._playMap[source._coordsKey] || dojo.dnd._empty)[this._coordsKey];
-  },
-  // _playMap should contain a mapping for each legal move, such that:
-  //   this._playMap['2,2']['3,3'] <==> true
-  // if the move from 2,2 => 3,3 is allowed
   updatePlayMap: function(plays) {
-    // needs to be shared across all instances
-    SquareSource.prototype._playMap = {};
+    // _playMap contains a mapping for each legal move, such that:
+    //   this._playMap['2,2']['3,3'] <==> true
+    // if the move from 2,2 => 3,3 is allowed
+    this._playMap = {};
     // iterate through plays array from server and build the map
     for (var i = 0; i < plays.length; i++) {
       var play = plays[i];
@@ -47,6 +33,25 @@ dojo.declare('SquareSource', dojo.dnd.Source, {
 
 // the global game object
 var game;
+
+dojo.require('dojo.dnd.Source');
+
+dojo.declare('SquareSource', dojo.dnd.Source, {
+  singular: true, // don't allow multiple selection regardless of key state
+  autoSync: true, // always sync up node list with square contents
+  constructor: function(node, params) {
+    // keep track of the coords for this square
+    this._coords = params.coords;
+  },
+  copyState: function() {
+    // never allow pieces to be copied
+    return false;
+  },
+  checkAcceptance: function(source) {
+    // whether the drop is a valid move
+    return game.isPlay(source._coords, this._coords);
+  }
+});
 
 dojo.addOnLoad(function() {
   // maps piece values to image urls
@@ -75,11 +80,11 @@ dojo.addOnLoad(function() {
       for (var x = 0; x <= 7; x++) {
         // test that the square is playable
         if ((x + y) % 2 == 0) {
+          // create playable square
           var td = dojo.create('td', {'class': 'on'}, tr);
           // look up piece image and insert image tag
-          var p = res.board[y][x];
-          var src = piece_images[p];
-          if (src) dojo.create('img', {'src': src, 'class': 'dojoDndItem'}, td);
+          var image = piece_images[game.getPiece(x, y)];
+          if (image) dojo.create('img', {'src': image, 'class': 'dojoDndItem'}, td);
           // create the drag/drop source
           new SquareSource(td, {coords: [x, y]});
         } else {
@@ -91,7 +96,7 @@ dojo.addOnLoad(function() {
   });
   // also update the play map
   xhr.addCallback(function(res) {
-    SquareSource.prototype.updatePlayMap(res.plays);
+    game.updatePlayMap(res.plays);
   });
   
   // listen for drops
