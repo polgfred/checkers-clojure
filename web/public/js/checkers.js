17e6eb59f34cb71c7f36a43c2484c3430e449e0c
Frederick Polgardy <fred@polgardy.com>
2009-03-11 19:16:50 -0500

    refactored board set up into game object

--- web/public/js/checkers.js
+++ web/public/js/checkers.js
@@ -1,17 +1,106 @@
+// the global game object - will be defined by addOnLoad
+var game;
+
+dojo.require('dojo.dnd.Source');
+dojo.declare('SquareSource', dojo.dnd.Source, {
+  singular: true, // don't allow multiple selection regardless of key state
+  autoSync: true, // always sync up node list with square contents
+  constructor: function(node, params) {
+    // keep track of the coords for this square
+    this._coords = params.coords;
+  },
+  copyState: function() {
+    // never allow pieces to be copied
+    return false;
+  },
+  checkAcceptance: function(source) {
+    // whether the drop is a valid move
+    return game.isPlay(source._coords, this._coords);
+  }
+});
+
 dojo.declare('Game', null, {
+  _pieceImages: {
+     '1': '/checkers/s/images/pb.png',
+     '2': '/checkers/s/images/kb.png',
+    '-1': '/checkers/s/images/pr.png',
+    '-2': '/checkers/s/images/kr.png'
+  },
+  _setupBoard: function() {
+    // create the board and set up targets
+    var table = dojo.byId('board');
+    // create the table rows
+    for (var y = 7; y >= 0; y--) {
+      var tr = dojo.create('tr', {}, table);
+      // create the table cells
+      for (var x = 0; x <= 7; x++) {
+        // test that the square is playable
+        if ((x + y) % 2 == 0) {
+          // create playable square
+          var td = dojo.create('td', {'class': 'on'}, tr);
+          // look up piece image and insert image tag
+          var image = this._pieceImages[this._board[y][x]];
+          if (image) dojo.create('img', {'src': image, 'class': 'dojoDndItem'}, td);
+          // create the drag/drop source
+          new SquareSource(td, {coords: [x, y]});
+        } else {
+          // create non-playable square
+          dojo.create('td', {'class': 'off'}, tr);
+        }
+      }
+    }
+  },
+  _getImg: function(x, y) {
+    var tr = dojo.query('#board tr')[7 - y];
+    var td = dojo.query('> td', tr)[x];
+    var nodes = dojo.query('> img', td);
+    if (nodes.length > 0) return nodes[0];
+  },
   constructor: function(side, board) {
     // keep track of the current position
     this._side = side;
     this._board = board;
-    // this._playMap = {};
-    // this._undoStack = [];
-  },
-  getPiece: function(x, y) {
-    return this._board[y][x];
+    this._plays = [];
+    this._setupBoard();
+    // listen for drops
+    dojo.subscribe('/dnd/drop', this, function(source) {
+      var target = dojo.dnd.manager().target;
+      this.doPlay(source._coords, target._coords);
+    });
   },
   doPlay: function(from, to) {
-    // perform the move
-    
+    // just in case, we'll check again
+    var playMap = this.isPlay(from, to);
+    if (!playMap) return;
+    // keep track of this move
+    if (this._plays.length == 0)
+      this._plays.push(from);
+    this._plays.push(to);
+    // move the piece
+    var x  = from[0];
+    var y  = from[1];
+    var nx = to[0];
+    var ny = to[1];
+    var p  = this._board[y][x];
+    this._board[y][x] = 0;
+    this._board[ny][nx] = p;
+    if (Math.abs(nx - x) == 2) {
+      // remove the jumped piece
+      var mx = (x + nx) / 2;
+      var my = (y + ny) / 2
+      this._board[my][mx] = 0;
+      dojo.destroy(this._getImg(mx, my));
+    }
+    // fire an event
+    if (playMap == true) {
+      // move complete
+      dojo.publish('/game/move/complete', [this._plays]);
+    } else if (playMap) {
+      // still your move
+      this._playMap = {};
+      this._playMap[to] = playMap;
+      dojo.publish('/game/move/partial', [this._plays]);
+    }
   },
   undoPlay: function(from, to, jumped) {
     // undo part of a move
@@ -39,78 +128,15 @@ dojo.declare('Game', null, {
   }
 });
 
-// the global game object - will be defined by addOnLoad
-var game;
-
-dojo.require('dojo.dnd.Source');
-
-dojo.declare('SquareSource', dojo.dnd.Source, {
-  singular: true, // don't allow multiple selection regardless of key state
-  autoSync: true, // always sync up node list with square contents
-  constructor: function(node, params) {
-    // keep track of the coords for this square
-    this._coords = params.coords;
-  },
-  copyState: function() {
-    // never allow pieces to be copied
-    return false;
-  },
-  checkAcceptance: function(source) {
-    // whether the drop is a valid move
-    return game.isPlay(source._coords, this._coords);
-  }
-});
-
 dojo.addOnLoad(function() {
-  // maps piece values to image urls
-  var piece_images = {
-     '1': '/checkers/s/images/pb.png',
-     '2': '/checkers/s/images/kb.png',
-    '-1': '/checkers/s/images/pr.png',
-    '-2': '/checkers/s/images/kr.png'
-  };
-  
   // get the new position on page load
   var xhr = dojo.xhrGet({
     url: '/checkers/new',
-    handleAs: 'json'
-  });
-  // build the board when the page first loads
-  xhr.addCallback(function(res) {
-    // set up the game object
-    game = new Game(res.side, res.board);
-    // create the board and set up targets
-    var board = dojo.byId('board');
-    // create the table rows
-    for (var y = 7; y >= 0; y--) {
-      var tr = dojo.create('tr', {}, board);
-      // create the table cells
-      for (var x = 0; x <= 7; x++) {
-        // test that the square is playable
-        if ((x + y) % 2 == 0) {
-          // create playable square
-          var td = dojo.create('td', {'class': 'on'}, tr);
-          // look up piece image and insert image tag
-          var image = piece_images[game.getPiece(x, y)];
-          if (image) dojo.create('img', {'src': image, 'class': 'dojoDndItem'}, td);
-          // create the drag/drop source
-          new SquareSource(td, {coords: [x, y]});
-        } else {
-          // create non-playable square
-          dojo.create('td', {'class': 'off'}, tr);
-        }
-      }
+    handleAs: 'json',
+    load: function(res) {
+      // set up the global game object
+      game = new Game(res.side, res.board);
+      game.updatePlayMap(res.plays);
     }
-  });
-  // also update the play map
-  xhr.addCallback(function(res) {
-    game.updatePlayMap(res.plays);
-  });
-  
-  // listen for drops
-  dojo.subscribe('/dnd/drop', function(source) {
-    // hook the drop event into the game object
-    var target = dojo.dnd.manager().target;
-    game.doPlay(source._coords, target._coords);
   });
 });
