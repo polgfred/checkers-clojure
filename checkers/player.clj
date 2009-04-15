a994aeb8e8e0ad110cc9a7f6a9a2b0ea5f1372bd
Frederick Polgardy <fred@polgardy.com>
2009-04-15 10:35:46 -0500

    docs! minor tweaks

--- checkers/player.clj
+++ checkers/player.clj
@@ -1,8 +1,8 @@
 (ns checkers.player (:use checkers.rules))
 
-(def *search-depth* 3)
+(def *search-depth* 3) ; initial depth for tree search
 
-(let [pb-vals
+(let [pb-vals ; black pieces
         [[ 100   0 104   0 104   0 100   0 ]
          [   0 100   0 102   0 102   0 100 ]
          [ 102   0 105   0 105   0 102   0 ]
@@ -11,9 +11,9 @@
          [   0 120   0 130   0 130   0 120 ]
          [ 130   0 142   0 142   0 130   0 ]
          [   0   0   0   0   0   0   0   0 ]]
-      pr-vals
+      pr-vals ; red pieces
         (vec (reverse (for [r pb-vals] (vec (reverse (map - r))))))
-      kb-vals
+      kb-vals ; black kings
         [[ 152   0 152   0 152   0 164   0 ]
          [   0 164   0 164   0 164   0 164 ]
          [ 152   0 180   0 180   0 164   0 ]
@@ -22,10 +22,13 @@
          [   0 164   0 180   0 180   0 152 ]
          [ 164   0 164   0 164   0 164   0 ]
          [   0 164   0 152   0 152   0 152 ]]
-      kr-vals
+      kr-vals ; red kings
         (vec (reverse (for [r kb-vals] (vec (reverse (map - r))))))]
 
   (defn calculate-score
+    "Given board `b', the sum of the scores of each piece's position in the above tables.
+
+    The red tables are defined to be the negated mirror image of the black tables."
     [b]
     (reduce +
       (for [[x y p] (squares b)]
@@ -38,26 +41,40 @@
 (declare best-play)
 
 (defn calculate-score-recursive
+  "Given board `b' and side `s', find the score of the current position.  If either the
+  maximum search depth has not been reached, or there are counter-jumps from this position,
+  return the score of the opposing side's best play; otherwise, calculate the score from
+  the value tables."
   [b s v]
   (if (or (pos? v) (seq (my-jumps b (- s))))
     (first (best-play b (- s) (dec v)))
     (calculate-score b)))
 
-(def compare-plays-fn
-  (memoize
-    (fn [s]
-      (if (= s +black+)
-        (fn ([] [-99999])
-            ([& plays]
-              (first (sort-by first > plays))))
-        (fn ([] [+99999])
-            ([& plays]
-              (first (sort-by first < plays))))))))
+(defn compare-plays-fn
+  "Given side `s', create a reducer that finds the best play from a list of plays.
+
+  This function is memoized for performance."
+  [s]
+  (if (= s +black+)
+    (fn
+      ([] [-99999])
+      ([& plays]
+        (first (sort-by first > plays))))
+    (fn
+      ([] [+99999])
+      ([& plays]
+        (first (sort-by first < plays))))))
+
+(def compare-plays-fn (memoize compare-plays-fn))
 
 (defn best-play-from
+  "Given board `b', the best play for side `s' from (x,y).  If `more' is non-empty,
+  then (x,y) is not a terminal position, so `best-play-from' must be invoked
+  recursively, and the best play selected from among its children.  The entire play
+  is accumulated into the value `play' as [x1 y1 x2 y2 ...]."
   [b s v play [x y & more]]
   (if (empty? more)
-    (let [play (reverse play)
+    (let [play (vec (reverse play))
           score (calculate-score-recursive b s v)]
       [score play])
     (reduce (compare-plays-fn s)
@@ -67,6 +84,7 @@
           (best-play-from b s v (conj play nx ny) tree))))))
 
 (defn best-play
+  "Given board `b', the `best' play for side `s' as [score (x1 y1 x2 y2 ...)]."
   ([b s] (best-play b s *search-depth*))
   ([b s v]
     (let [plays (my-plays b s)]
--- test/player_test.clj
+++ test/player_test.clj
@@ -21,7 +21,7 @@
           [  0  0  0  0  0  0  0  0  ]]]
 
   (let [[score play] (best-play b +black+)]
-    (assert (= play '(2 0 4 2 6 4 4 6)))))
+    (assert (= play [2 0 4 2 6 4 4 6]))))
 
 (let [b  [[  0  0  1  0  0  0  0  0  ]
           [  0  0  0 -1  0  0  0  0  ]
@@ -34,4 +34,4 @@
 
   (let [[score play] (best-play b +black+)]
     (assert* (= score 99999)
-             (= play '(2 0 4 2)))))
+             (= play [2 0 4 2]))))
